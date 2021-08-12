5b2bef9d028d41936a8c8b88f79497eed24a077f
hauphanlvc <hauphanlvc@gmail.com>
2021-08-13 04:27:44 +0700

    upload all code

--- sever.py
+++ sever.py
@@ -1,6 +1,7 @@
 import pickle
 from flask import Flask, render_template, request
 import os
+from random import random
 import cv2
 import  sys
 from tensorflow.keras.applications.vgg16 import VGG16
@@ -9,11 +10,13 @@ from tensorflow.keras.applications.vgg16 import preprocess_input
 import numpy as np  
 from keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img  
 def TrichXuatFeatures(file):
+  model_VGG16 = VGG16(weights='imagenet', include_top=False)
+  from keras.preprocessing.image import img_to_array, load_img  
   img = image.load_img(file, target_size=(224, 224)) # chuyển ảnh về size (224,224)
-  x = image.img_to_array(img)        # chuyển ảnh về thành 1 array 
-  x = x.reshape((1, x.shape[0], x.shape[1], x.shape[2])) 
+  x = image.img_to_array(img)        # chuyển ảnh về thành 1 array
+  x = x.reshape((1, x.shape[0], x.shape[1], x.shape[2]))
   x = preprocess_input(x)
-  features = model.predict(x)
+  features = model_VGG16.predict(x)
   features = np.array(features).reshape(-1,1)
   return features
 def DuDoan1AnhVGG16(model,file): # dự đoán 1 ảnh dựa theo cách trích xuất VGG16
@@ -27,45 +30,38 @@ def DuDoan1AnhVGG16(model,file): # dự đoán 1 ảnh dựa theo cách trích x
   return y_pred
 # Khởi tạo Flask
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = "FolderImage"
+app.config['UPLOAD_FOLDER'] = "static"
 
 filename = 'model.sav'
-model = pickle.load(open(filename, 'rb'))
+model_ = pickle.load(open(filename, 'rb'))
 
 
 
-# Hàm xử lý reques
+# Hàm xử lý request
 @app.route("/", methods=['GET', 'POST'])
 def home_page():
     # Nếu là POST (gửi file)
     if request.method == "POST":
-         try:
+         
             # Lấy file gửi lên
-            image = request.files['file']
-            if image:
+            image_ = request.files['file']
+            
                 # Lưu file
-                print(image.filename)
-                print(app.config['UPLOAD_FOLDER'])
-                path_to_save = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
-                print("Save = ", path_to_save)
-                image.save(path_to_save)
+            dirs_image = os.listdir(app.config['UPLOAD_FOLDER'])
+            print(image_.filename)
+            print(app.config['UPLOAD_FOLDER'])
+            path_to_save = os.path.join(app.config['UPLOAD_FOLDER'], image_.filename)
+            for file_ in dirs_image:
+              if file_ == image_.filename:
+                res = DuDoan1AnhVGG16(model_,path_to_save)
+                print(image_.filename)
+                # return render_template("index.html", user_image = image.filename , result = res)
+                
+        
+            print("Save = ", path_to_save)
+            image_.save(path_to_save)                                        
+            return render_template("index.html",  msg = str(res[0]),user_image = image_.filename)
 
-                # Convert image to dest size tensor
-                res = DuDoan1AnhVGG16(model,path_to_save)
-
-
-
-                return render_template("index.html", user_image = image.filename , result = res)
-                # else:
-                #     return render_template('index.html', msg='Không nhận diện được khuôn mặt')
-            else:
-                # Nếu không có file thì yêu cầu tải file
-                return render_template('index.html', msg='Hãy chọn file để tải lên')
-
-         except Exception as ex:
-            # Nếu lỗi thì thông báo
-            print(ex)
-            return render_template('index.html', msg='Không nhận diện được khuôn mặt')
 
     else:
         # Nếu là GET thì hiển thị giao diện upload
