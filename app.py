4ef51ebc70079200d4659fd3007dfd7312b86f8b
hauphanlvc <hauphanlvc@gmail.com>
2021-08-14 00:43:52 +0700

    upload code

--- app.py
+++ app.py
@@ -68,6 +68,7 @@ def home_page():
         return render_template('index.html')
 
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
 #     # app.run(host='0.0.0.0', debug=True)
 #     app.run()
+      app.run(host="0.0.0.0")
