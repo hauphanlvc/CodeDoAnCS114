672975f7690bfdfde7ac74331caa82be77cb5f16
hauphanlvc <hauphanlvc@gmail.com>
2021-08-15 14:00:58 +0700

    upload code version 4

--- app.py
+++ app.py
@@ -72,5 +72,6 @@ def home_page():
 
 if __name__ == '__main__':
 #     app.run(host='0.0.0.0', debug=True)
-    app.run()
+    # app.run()
       # app.run(host="0.0.0.0")
+    app.run(debug=True,host='0.0.0.0', port=9007)
